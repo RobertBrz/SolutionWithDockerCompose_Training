8c58dc77a621aafe5cf6fa959625a16a9812671b
robert.brzozowski <noemail>
2023-01-04 11:16:08 +0100

    console procuder and consumer and docker compose file

---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Sockets;
+
+internal class Program
+{
+    static TcpClient tcpClient = new TcpClient();
+
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            if (tcpClient.Connected) Send();
+            else
+            {
+                try
+                {
+                    tcpClient.Connect("Consumer", 82);
+                }
+                catch (SocketException e) { 
+                Console.WriteLine(e.ToString());    
+                }
+            }
+
+            Task.Delay(2000).Wait();
+        }
+    }
+
+    private static void Send()
+    {
+       Random random= new Random(); 
+        string message  = random.Next(0,99).ToString();
+        Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+        NetworkStream stream = tcpClient.GetStream();
+        stream.Write(data, 0, data.Length);
+    }
+}
